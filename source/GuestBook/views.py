914f2c4ccb03b916f1cb9a4d6bf7647314e09e17
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-01-08 15:51:36 +0600

    Создал base.html подключил bootstrap и bootstrap-icons, в index.html добавил разметку и вывод записей, добавил в представлении сортировку и фильтр по активным гостям

--- source/GuestBook/views.py
+++ source/GuestBook/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from GuestBook.models import Guest, STATUS_CHOICES
 
 # Create your views here.
 
 
 def index_view(request):
-    return render(request, 'index.html')
+    guest = Guest.objects.order_by("-created_at").filter(status="active")
+    context = {'guests': guest}
+    return render(request, 'index.html', context)
