2d1a8a18a86fd08c37cf934e262c5fa008a380ff
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-01-08 13:43:30 +0600

    Внес изменения в admin.py, создал фикстуру dump.json

--- source/GuestBook/admin.py
+++ source/GuestBook/admin.py
@@ -8,8 +8,9 @@ class GuestAdmin(admin.ModelAdmin):
     list_display = ['id', 'status', 'author_name', 'created_at']
     list_filter = ['status']
     search_fields = ['status']
-    fields = ['author_name', 'author_mail', 'text_notes']
+    fields = ['status', 'author_name', 'author_mail', 'text_notes']
     readonly_fields = ['created_at']
 
 
 admin.site.register(Guest, GuestAdmin)
+
