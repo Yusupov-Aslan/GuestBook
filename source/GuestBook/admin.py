3b7a9ae99422bf51a54f2ac1ad4d02dfff242884
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-01-08 12:54:51 +0600

    В settings.py добавил приложение GuestBook, создал модель Guest и сделал миграцию, создал urls.py, зарегистрировал модель в admin.py

--- source/GuestBook/admin.py
+++ source/GuestBook/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from GuestBook.models import Guest
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status', 'author_name', 'created_at']
+    list_filter = ['status']
+    search_fields = ['status']
+    fields = ['status', 'author_name', 'created_at', 'editing_time']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Guest, GuestAdmin)
--- source/GuestBook/models.py
+++ source/GuestBook/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+STATUS_CHOICES = [("active", "Активно"), ("blocked", "Заблокировано")]
+
+
+class Guest(models.Model):
+    author_name = models.CharField(max_length=30, null=True, blank=True, verbose_name='Имя автора')
+    author_mail = models.EmailField(max_length=100, null=False, blank=False, verbose_name="Почта автора")
+    text_notes = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Текст записи')
+    status = models.CharField(max_length=20, default='active', choices=STATUS_CHOICES, verbose_name='Статус')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    editing_time = models.DateTimeField(auto_now=True, verbose_name="Время редактирования")
+
+    def __str__(self):
+        return f"{self.pk}. {self.author_name}: {self.status}"
+
+    class Meta:
+        db_table = 'guest'
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
